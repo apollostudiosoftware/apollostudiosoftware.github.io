27b1522a9a7dd7298da4401d93cab6dd9bb60b2d
Sólon <solon@apollo.studio>
2023-03-01 13:32:01 +0000

    launch!

---
+++ script.js
@@ -0,0 +1,16 @@
+/* var granimInstance = new Granim({
+    element: '#canvas-basic',
+    name: 'basic-gradient',
+    direction: 'top-bottom',
+    opacity: [1, 1],
+    isPausedWhenNotInView: true,
+    states : {
+        "default-state": {
+            gradients: [
+                ['#00241D', '#002324'],
+                ['#001B24', '#00241D']
+            ]
+        }
+    }
+}); */
+
